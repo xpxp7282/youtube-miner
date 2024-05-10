c53495103eb29c8f9168ea23ff278a964c57e5d3
thomas <thomas.dessolier@etudiant.univ-rennes1.fr>
2024-05-10 10:00:02 +0200

    ok

--- build-multiarch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ARCH=$1
-
-if [ "$ARCH" == "amd64" ]; then
-    docker buildx build --platform linux/amd64 -t youtube-miner:$ARCH .
-else
-    docker buildx build --platform linux/arm64 -t youtube-miner:$ARCH .
-fi
